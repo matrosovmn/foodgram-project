b6a3d15a5bf0f7f21be82032661819bdd43d1ffa
Михаил Матросов <matrosovmn@gmail.comd:/VS Code/Yandex Practicum/  /venv/Scripts/python.exe>
2023-08-26 19:23:53 +0500

    redefined the model User

--- backend/users/models.py
+++ backend/users/models.py
@@ -39,6 +39,9 @@ class CustomUser(AbstractUser):
         help_text="Введите адрес электронной почты для регистрации.",
     )
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
