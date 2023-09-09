0d900a864694fac92b4fe9231f8effdc992a80c4
Михаил Матросов <matrosovmn@gmail.comd:/VS Code/Yandex Practicum/  /venv/Scripts/python.exe>
2023-09-09 13:35:18 +0500

    test4 Tags

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
-from rest_framework import filters, status, viewsets
+from rest_framework import filters, status, viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
@@ -106,7 +106,7 @@ class TagViewSet(ReadOnlyModelViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = None
+    permission_classes = (permissions.AllowAny,)
     # pagination_class = None
 
 
