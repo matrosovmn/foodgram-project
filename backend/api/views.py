7d53a7ac1ef2d478c8becf22bb0733132c868986
Михаил Матросов <matrosovmn@gmail.comd:/VS Code/Yandex Practicum/  /venv/Scripts/python.exe>
2023-09-17 17:55:11 +0500

    test sites

--- backend/api/views.py
+++ backend/api/views.py
@@ -114,7 +114,7 @@ class TagViewSet(ReadOnlyModelViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAdminOrReadOnly,)
     pagination_class = None
 
 
